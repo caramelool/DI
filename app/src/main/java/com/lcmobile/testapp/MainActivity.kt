9b0cc60ca8b7f1d656dff66d96ec65ea42e4837a
caramelool <lucas.caramelo@mercadolivre.com>
2019-12-23 19:19:04 -0300

    activity/fragment injectable

--- app/src/main/java/com/lcmobile/testapp/MainActivity.kt
+++ app/src/main/java/com/lcmobile/testapp/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.lcmobile.testapp
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import com.lcmobile.di.Injectable
-import com.lcmobile.di.Module
 import com.lcmobile.di.moduleOf
-import java.lang.IllegalStateException
 
 val mainModule = moduleOf {
     provide {
@@ -21,16 +17,12 @@ class MainActivity : AppCompatActivity(), Injectable {
     override val module by mainModule
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        withContext(applicationContext)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
 
 class MainFragment : Fragment(), Injectable {
-
     override val module by mainModule
-
-    override fun getApplicationContext(): Context {
-        return activity?.applicationContext ?: throw IllegalStateException()
-    }
 }
